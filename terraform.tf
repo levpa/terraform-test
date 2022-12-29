2380769b2d78870028a1799c5a453888cb19baa2
Lev Pasichnyi <lev.pasichnyi@lPasichnyi-m1.local>
2022-12-29 18:13:53 +0200

    my first commit

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 
   cloud {
-    organization = "organization-name"
+    organization = "levpa"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "test"
     }
   }
 

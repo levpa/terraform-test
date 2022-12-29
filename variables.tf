73a83071db093765eb35879c027335ff38ab32e3
Lev Pasichnyi <lev.pasichnyi@iconplc.com>
2022-12-29 19:43:03 +0200

    test terraform

--- variables.tf
+++ variables.tf
@@ -12,6 +12,3 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
